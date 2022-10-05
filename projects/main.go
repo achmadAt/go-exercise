7d06868b1d99d297bdc9e69203fc8078c9377b9e
achmad <achmadalfazari@gmail.com>
2022-10-05 10:01:07 +0700

    feat: initiate todos and change main projects

--- projects/main.go
+++ projects/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"projects/config"
 
 	"github.com/labstack/echo/v4"
@@ -13,7 +15,14 @@ type HelloWorld struct {
 
 func main() {
 	e := echo.New()
+	//e.POST("/user", saveUser)
+	e.GET("user/:id", getUser)
+	//e.PUT("/user/:id", updateUser)
+	//e.DELETE("/user/:id", deleteUser)
 	e.GET("/", Greetings)
+	e.GET("/show", show)
+	e.POST("/save", save)
+	e.POST("/save2", save2)
 	e.Logger.Fatal(e.Start(config.Port))
 }
 
@@ -21,4 +30,52 @@ func Greetings(c echo.Context) error {
 	return c.JSON(http.StatusOK, HelloWorld{
 		Message: "Hello World",
 	})
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func save(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+name+", email:"+email+"\n")
+}
+
+func save2(c echo.Context) error {
+	// Get name
+	name := c.FormValue("name")
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
---
+++ todo/entities/todos.go
@@ -0,0 +1,7 @@
+package entities
+
+type ToDo struct {
+	ID int
+	Task string
+	status string
+}
