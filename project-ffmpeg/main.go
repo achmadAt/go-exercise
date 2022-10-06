b0918655f3c61794b7fb003eb89b686a22d438bb
achmad <achmadalfazari@gmail.com>
2022-10-06 14:07:08 +0700

    feat: show ffpmeg help with exec

--- project-ffmpeg/main.go
+++ project-ffmpeg/main.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
-	cmd := "ffmpeg -i test.mp4 -vf \"fps=5,scale=320:-1:flags=lanczos\" -c:v pam -f image2pipe - | convert -delay 5 - -loop 0 -layers optimize test.gif"
-	_, err := exec.Command("bash", "-c", cmd).Output()
+	cmd := "ffmpeg"
+	//cmd := "ffmpeg -i test.mp4 -vf \"fps=5,scale=320:-1:flags=lanczos\" -c:v pam -f image2pipe - | convert -delay 5 - -loop 0 -layers optimize test.gif"
+	Output, err := exec.Command("ffmpeg", "--help").Output()
 	if err != nil {
-		fmt.Printf("failed to execute commdand %s", cmd)
+		fmt.Println(err)
+		fmt.Printf("failed to execute commdand %s\n", cmd)
+	}
+	if Output != nil {
+		str := Output
+		str1 := string(str[:])
+		fmt.Println(str1)
 	}
 }
