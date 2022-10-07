6fc0312c41da792cbdc8cd30c1aaf4cd51b9185e
achmad <achmadalfazari@gmail.com>
2022-10-07 16:12:47 +0700

    fix: fix null value

--- echo-mongo/controller/todo.go
+++ echo-mongo/controller/todo.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"echo-mongo/config"
 	"echo-mongo/dto"
+	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -17,6 +18,7 @@ var validate = validator.New()
 
 func CreateTodo(c echo.Context) error {
 	var todo dto.Todo
+	todo.Name = c.FormValue("name")
 	if err := c.Bind(&todo); err != nil {
 		return c.JSON(http.StatusBadRequest, dto.Response{Status: http.StatusBadRequest, Message: "error", Data: &echo.Map{"data": err.Error()}})
 	}
@@ -31,6 +33,7 @@ func CreateTodo(c echo.Context) error {
 	if err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
+	fmt.Println(todo.Name)
 	return c.JSON(http.StatusCreated, dto.Response{Status: http.StatusCreated, Message: "success", Data: &echo.Map{"data": result}})
 }
 
