d16296617c0f7449c2472782c118e73d002e003f
achmad <achmadalfazari@gmail.com>
2022-10-22 01:00:44 +0700

    feat: finish fixing image orientation

--- image-processing/main.go
+++ image-processing/main.go
@@ -1,161 +1,164 @@
 package main
 
 import (
-	"bytes"
-	"image"
-	"io/ioutil"
-	"os"
-
-	"errors"
-	"image/color"
-	"image/gif"
-	"image/jpeg"
-	"image/png"
-	"strings"
+	//"os"
 
 	"fmt"
-
-	"github.com/disintegration/imaging" // go get -u github.com/disintegration/imaging
-	"github.com/rwcarlsen/goexif/exif"  // go get -u github.com/rwcarlsen/goexif/exif
-	exifremove "github.com/scottleedavis/go-exif-remove"
-	"github.com/sirupsen/logrus" // go get -u github.com/sirupsen/logrus
+	// go get -u github.com/disintegration/imaging
+	//"github.com/rwcarlsen/goexif/exif" // go get -u github.com/rwcarlsen/goexif/exif
+	//"github.com/rwcarlsen/goexif/mknote"
+	// go get -u github.com/sirupsen/logrus
 )
 
 func main() {
-	data, err := ioutil.ReadFile("download.jpeg")
+	fmt.Println("hello")
+	// data, err := ioutil.ReadFile("download.jpeg")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	exif, err := setupExifTool()
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer exif.Shutdown()
+	fixMetaJPEG("download.jpeg", "img2.jpeg")
+	a := rawOrientation("img.JPG")
+	fmt.Println(a)
+	addOrientation("download.jpeg")
+	// filtered, err := exifremove.Remove(data)
+	// if err != nil {
+	// 	fmt.Printf("* " + err.Error() + "\n")
+	// }
+	// serveImage(filtered)
+	// fmt.Println(filtered)
 
-	filtered, err := exifremove.Remove(data)
-	if err != nil {
-		fmt.Printf("* " + err.Error() + "\n")
-	}
-	serveImage(filtered)
-	fmt.Println(filtered)
-	file, err := os.Open("download.jpeg")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-	image, err := jpeg.Decode(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	im, err := exif.Decode(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(im.Raw)
-	imaging.Rotate(image, float64(180), color.Gray16{})
-	imaging.Fit(image, 200, 200, imaging.Lanczos)
+	// file, err := os.Open("img.JPG")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer file.Close()
+	// //image, err := jpeg.Decode(file)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// exif.RegisterParsers(mknote.All...)
+	// im, err := exif.Decode(file)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(im.Raw)
+	//serveImage(im.Raw)
+	//imaging.Rotate(image, float64(180), color.Gray16{})
+	//imaging.Fit(image, 200, 200, imaging.Lanczos)
 	// reverseOrientation(image, "2")
 }
-func serveImage(imgByte []byte) {
 
-	img, _, err := image.Decode(bytes.NewReader(imgByte))
-	if err != nil {
-		fmt.Println(err)
-	}
+// func serveImage(imgByte []byte) {
 
-	out, _ := os.Create("img.jpeg")
-	defer out.Close()
+// 	img, _, err := image.Decode(bytes.NewReader(imgByte))
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	var opts jpeg.Options
-	opts.Quality = 1
+// 	out, _ := os.Create("img2.jpeg")
+// 	defer out.Close()
 
-	err = jpeg.Encode(out, img, &opts)
-	//jpeg.Encode(out, img, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-func ReadImage(fpath string) *image.Image {
-	var img image.Image
-	var err error
-	// deal with image
-	ifile, err := os.Open(fpath)
-	if err != nil {
-		logrus.Warnf("could not open file for image transformation: %s", fpath)
-		return nil
-	}
-	defer ifile.Close()
-	filetype, err := GetSuffix(fpath)
-	if err != nil {
-		return nil
-	}
-	if filetype == "jpg" || filetype == "jpeg" {
-		img, err = jpeg.Decode(ifile)
-		if err != nil {
-			return nil
-		}
-	} else if filetype == "png" {
-		img, err = png.Decode(ifile)
-		if err != nil {
-			return nil
-		}
-	} else if filetype == "gif" {
-		img, err = gif.Decode(ifile)
-		if err != nil {
-			return nil
-		}
-	}
-	// deal with exif
-	efile, err := os.Open(fpath)
-	if err != nil {
-		logrus.Warnf("could not open file for exif decoder: %s", fpath)
-	}
-	defer efile.Close()
-	x, err := exif.Decode(efile)
-	if err != nil {
-		if x == nil {
-			// ignore - image exif data has been already stripped
-		}
-		logrus.Errorf("failed reading exif data in [%s]: %s", fpath, err.Error())
-	}
-	if x != nil {
-		orient, _ := x.Get(exif.Orientation)
-		if orient != nil {
-			logrus.Infof("%s had orientation %s", fpath, orient.String())
-			img = reverseOrientation(img, orient.String())
-		} else {
-			logrus.Warnf("%s had no orientation - implying 1", fpath)
-			img = reverseOrientation(img, "1")
-		}
-		imaging.Save(img, fpath)
-	}
-	return &img
-}
+// 	var opts jpeg.Options
+// 	opts.Quality = 20
 
-// reverseOrientation amply`s what ever operation is necessary to transform given orientation
-// to the orientation 1
-func reverseOrientation(img image.Image, o string) *image.NRGBA {
-	switch o {
-	case "1":
-		return imaging.Clone(img)
-	case "2":
-		return imaging.FlipV(img)
-	case "3":
-		return imaging.Rotate180(img)
-	case "4":
-		return imaging.Rotate180(imaging.FlipV(img))
-	case "5":
-		return imaging.Rotate270(imaging.FlipV(img))
-	case "6":
-		return imaging.Rotate270(img)
-	case "7":
-		return imaging.Rotate90(imaging.FlipV(img))
-	case "8":
-		return imaging.Rotate90(img)
-	}
-	logrus.Errorf("unknown orientation %s, expect 1-8", o)
-	return imaging.Clone(img)
-}
+// 	err = jpeg.Encode(out, img, &opts)
+// 	//jpeg.Encode(out, img, nil)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
 
-func GetSuffix(name string) (string, error) {
-	if !strings.Contains(name, ".") {
-		return name, errors.New("file names without file type suffix are not supported")
-	}
-	split := strings.Split(name, ".")
-	return strings.ToLower(strings.TrimSpace(split[len(split)-1])), nil
-}
+// func ReadImage(fpath string) *image.Image {
+// 	var img image.Image
+// 	var err error
+// 	// deal with image
+// 	ifile, err := os.Open(fpath)
+// 	if err != nil {
+// 		logrus.Warnf("could not open file for image transformation: %s", fpath)
+// 		return nil
+// 	}
+// 	defer ifile.Close()
+// 	filetype, err := GetSuffix(fpath)
+// 	if err != nil {
+// 		return nil
+// 	}
+// 	if filetype == "jpg" || filetype == "jpeg" {
+// 		img, err = jpeg.Decode(ifile)
+// 		if err != nil {
+// 			return nil
+// 		}
+// 	} else if filetype == "png" {
+// 		img, err = png.Decode(ifile)
+// 		if err != nil {
+// 			return nil
+// 		}
+// 	} else if filetype == "gif" {
+// 		img, err = gif.Decode(ifile)
+// 		if err != nil {
+// 			return nil
+// 		}
+// 	}
+// 	// deal with exif
+// 	efile, err := os.Open(fpath)
+// 	if err != nil {
+// 		logrus.Warnf("could not open file for exif decoder: %s", fpath)
+// 	}
+// 	defer efile.Close()
+// 	x, err := exif.Decode(efile)
+// 	if err != nil {
+// 		if x == nil {
+// 			// ignore - image exif data has been already stripped
+// 		}
+// 		logrus.Errorf("failed reading exif data in [%s]: %s", fpath, err.Error())
+// 	}
+// 	if x != nil {
+// 		orient, _ := x.Get(exif.Orientation)
+// 		if orient != nil {
+// 			logrus.Infof("%s had orientation %s", fpath, orient.String())
+// 			img = reverseOrientation(img, orient.String())
+// 		} else {
+// 			logrus.Warnf("%s had no orientation - implying 1", fpath)
+// 			img = reverseOrientation(img, "1")
+// 		}
+// 		imaging.Save(img, fpath)
+// 	}
+// 	return &img
+// }
+
+// // reverseOrientation amply`s what ever operation is necessary to transform given orientation
+// // to the orientation 1
+// func reverseOrientation(img image.Image, o string) *image.NRGBA {
+// 	switch o {
+// 	case "1":
+// 		return imaging.Clone(img)
+// 	case "2":
+// 		return imaging.FlipV(img)
+// 	case "3":
+// 		return imaging.Rotate180(img)
+// 	case "4":
+// 		return imaging.Rotate180(imaging.FlipV(img))
+// 	case "5":
+// 		return imaging.Rotate270(imaging.FlipV(img))
+// 	case "6":
+// 		return imaging.Rotate270(img)
+// 	case "7":
+// 		return imaging.Rotate90(imaging.FlipV(img))
+// 	case "8":
+// 		return imaging.Rotate90(img)
+// 	}
+// 	logrus.Errorf("unknown orientation %s, expect 1-8", o)
+// 	return imaging.Clone(img)
+// }
+
+// func GetSuffix(name string) (string, error) {
+// 	if !strings.Contains(name, ".") {
+// 		return name, errors.New("file names without file type suffix are not supported")
+// 	}
+// 	split := strings.Split(name, ".")
+// 	return strings.ToLower(strings.TrimSpace(split[len(split)-1])), nil
+// }
